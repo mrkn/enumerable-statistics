9075e1ed325e4a0688336c00d5e5177564253428
Kenta Murata <mrkn@mrkn.jp>
2019-01-12 22:05:32 +0900

    extconf.rb: examine rational and complex functions

--- ext/enumerable/statistics/extension/extconf.rb
+++ ext/enumerable/statistics/extension/extconf.rb
@@ -1,5 +1,17 @@
 require 'mkmf'
 
 have_type('struct RRational')
+have_func('rb_rational_new')
+have_func('rb_rational_num')
+have_func('rb_rational_den')
+have_func('rb_rational_plus')
+
 have_type('struct RComplex')
+have_func('rb_complex_raw')
+have_func('rb_complex_real')
+have_func('rb_complex_imag')
+have_func('rb_complex_plus')
+have_func('rb_complex_div')
+have_func('rb_dbl_complex_new')
+
 create_makefile('enumerable/statistics/extension')
